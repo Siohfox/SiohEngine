9b63dafeb4ee18813ad9c7c3d2aca387b32a7c48
Siohfox <siohfox@gmail.com>
2022-09-26 14:51:36 +0100

    Initial Commit

---
+++ src/game/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Please enter how much moneys you give us:\n";
+	
+	int money;
+
+	std::cin >> money;
+
+	std::cout << "Wow look you gave us $" << money;
+
+	system("tree C:");
+
+	std::cout << '\a';
+	std::cout << "uh oh yro'ue computer has virus!!!!\ninsert 1009USD because we know who u r";
+	system("whoami");
+
+	std::cin.get();
+
+	return 0;
+}
